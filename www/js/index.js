73f18a3dd37281b0427d304e6791645e15257447
behnam2019 <behnam2019@gmail.com>
2017-05-31 13:49:51 -0700

    Update index.js

--- www/js/index.js
+++ www/js/index.js
@@ -13,10 +13,10 @@ function barcodescanner(){
           alert("Scanning failed: " + error);
       },
       {
-          preferFrontCamera : true, // iOS and Android
+          preferFrontCamera : false, // iOS and Android
           showFlipCameraButton : true, // iOS and Android
           showTorchButton : true, // iOS and Android
-          torchOn: true, // Android, launch with the torch switched on (if available)
+          torchOn: false, // Android, launch with the torch switched on (if available)
           prompt : "Place a barcode inside the scan area", // Android
           resultDisplayDuration: 500, // Android, display scanned text for X ms. 0 suppresses it entirely, default 1500
           formats : "QR_CODE,PDF_417", // default: all but PDF_417 and RSS_EXPANDED
